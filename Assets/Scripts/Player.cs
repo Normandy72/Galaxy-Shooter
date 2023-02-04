13cb632d2ce292a63babff04760ef399a6f63406
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-02-04 16:45:31 +0100

    cleanup code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,6 +15,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         // --- user input ---
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -24,18 +29,20 @@ public class Player : MonoBehaviour
         // Vector3.up == new Vector3(0, 1, 0);
         //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
         //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
-
         transform.Translate(direction * _speed * Time.deltaTime);
 
+
         // --- bounds ---
-        if(transform.position.y >= 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-        }
-        else if(transform.position.y <= -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
-        }
+        // if(transform.position.y >= 0)
+        // {
+        //     transform.position = new Vector3(transform.position.x, 0, 0);
+        // }
+        // else if(transform.position.y <= -3.8f)
+        // {
+        //     transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        // }
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
 
         if(transform.position.x > 11.3f)
         {
