b00c26d05cd4c0c0db37b793b6b73d18bd21ee2b
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-02-06 20:34:21 +0100

    add GAME OVER text

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,12 +7,14 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private TextMeshProUGUI _gameOverText;
     [SerializeField] private Sprite[] _liveSprites;
     [SerializeField] private Image _livesImg;
 
     void Start()
     {
         _scoreText.text = "Score: " + 0;
+        _gameOverText.gameObject.SetActive(false);
     }
 
     public void UpdateScore(int playerScore)
@@ -23,5 +25,10 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int currentLives)
     {
         _livesImg.sprite = _liveSprites[currentLives];
+
+        if(currentLives == 0)
+        {
+            _gameOverText.gameObject.SetActive(true);
+        }
     }
 }
