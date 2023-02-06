a268460cda494feba7a441a0bcbf88af2d5646fd
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-02-06 20:12:53 +0100

    add lives display

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private Sprite[] _liveSprites;
+    [SerializeField] private Image _livesImg;
 
     void Start()
     {
@@ -15,5 +18,10 @@ public class UIManager : MonoBehaviour
     public void UpdateScore(int playerScore)
     {
         _scoreText.text = "Score: " + playerScore;
+    }
+
+    public void UpdateLives(int currentLives)
+    {
+        _livesImg.sprite = _liveSprites[currentLives];
     }
 }
