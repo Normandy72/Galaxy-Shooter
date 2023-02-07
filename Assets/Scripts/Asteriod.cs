673157d5272a4cf9be82239b5fe0bc3756ad75d9
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-02-07 21:01:32 +0100

    explode the asteroid

--- Assets/Scripts/Asteriod.cs
+++ Assets/Scripts/Asteriod.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Asteriod : MonoBehaviour
 {
     [SerializeField] private float _rotateSpeed = 3.0f;
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] private GameObject _explosionPrefab;
+    
     void Update()
     {
         transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject, 0.25f);
+        }
     }
 }
