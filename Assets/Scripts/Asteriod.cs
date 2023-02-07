50127ddea54313ec46b245c3d646494928afba40
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-02-07 21:27:13 +0100

    controlling the spawn wave through the asteriod

--- Assets/Scripts/Asteriod.cs
+++ Assets/Scripts/Asteriod.cs
@@ -6,6 +6,12 @@ public class Asteriod : MonoBehaviour
 {
     [SerializeField] private float _rotateSpeed = 3.0f;
     [SerializeField] private GameObject _explosionPrefab;
+    private SpawnManager _spawnManager;
+
+    void Start()
+    {
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+    }
     
     void Update()
     {
@@ -18,6 +24,7 @@ public class Asteriod : MonoBehaviour
         {
             Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
             Destroy(other.gameObject);
+            _spawnManager.StartSpawning();
             Destroy(this.gameObject, 0.25f);
         }
     }
